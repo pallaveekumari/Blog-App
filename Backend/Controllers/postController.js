f7b17f95379abafe26536ff34262206c3f8f8f96
pallavee <pallaveeratnakar1714@gmail.com>
2023-04-08 18:07:15 +0530

    liked_and_unliked_api_done

--- Backend/Controllers/postController.js
+++ Backend/Controllers/postController.js
@@ -41,15 +41,44 @@ const deletePost=async (req,res)=>{
     const {id}=req.params;
     try{
 const deletedPost= await postModel.findByIdAndDelete({_id:id})
-    }
+res.status(200).json({msg:"Deleted Post Successfully",deletedPost})   
+}
     catch(err)
     {
         res.status(400).json({msg:"Post Not Found",error:err})
     }
 }
+
+
+const likePost=async (req,res)=>{
+    const {id}=req.params;
+    try{
+const likedPost= await postModel.findOneAndUpdate({_id:id})
+res.status(200).json({msg:"Liked Post Successfully",likedPost})
+    }
+    catch(err)
+    {
+res.status(400).json({msg:"Something went wrong",error:err})
+    }
+}
+
+
+const unLikePost=async (req,res)=>{
+    const {id}=req.params;
+    try{
+        const unLikedPost= await postModel.findOneAndUpdate({_id:id})
+        res.status(200).json({msg:"UnLiked Post Successfully",unLikedPost})
+    }
+    catch(err)
+    {
+        res.status(400).json({msg:"Something went wrong",error:err})   
+    }
+}
 module.exports={
     retrievePost,
     updatePost,
-    deletePost
+    deletePost,
+    likePost,
+    unLikePost
 
 }
